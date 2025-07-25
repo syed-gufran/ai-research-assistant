4efbbe82fe2c4dab132822684f0d297897f2abee
syed-gufran <tazeema07@gmail.com>
2025-07-25 12:25:30 +0530

    created a ai research agent

---
+++ agents/content_writer.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from crewai import Agent, LLM
+from crewai_tools import FileWriterTool
+
+
+# LLM configurations - Agent specific config
+model = os.getenv("WRITER_AGENT_LLM")
+temperature = float(os.getenv("WRITER_AGENT_TEMPERATURE"))
+
+llm = LLM(
+    model=model,
+    temperature=temperature
+)
+
+content_writer_agent = Agent(
+    role="Content Writer",
+    goal="Create comprehensive, well-structured reports and summaries",
+    backstory = (
+                "You are a professional content writer with expertise in creating "
+                "clear, engaging, and well-structured documents. You can transform complex "
+                "information into accessible and compelling content."
+            ),
+    llm=llm,
+    tools=[FileWriterTool()],
+    verbose=True,
+)
---
+++ agents/research_specialist.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from crewai import Agent, LLM
+from crewai_tools import SerperDevTool
+
+
+# LLM configurations - Agent specific config
+model = os.getenv("RESEARCH_AGENT_LLM")
+temperature = float(os.getenv("RESEARCH_AGENT_TEMPERATURE"))
+
+llm = LLM(
+    model=model,
+    temperature=temperature
+)
+
+research_specialist_agent = Agent(
+    role="Research Specialist",
+    goal="Gather comprehensive and accurate information on given topics from multiple sources",
+    backstory = (
+                "You are an expert research specialist with years of experience in information gathering "
+                "and fact-checking. You have a keen eye for reliable sources and can quickly identify the "
+                "most relevant and up-to-date information on any topic."
+            ),
+    llm=llm,
+    tools=[SerperDevTool()],
+    verbose=True,
+)
